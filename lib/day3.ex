03e74deb59297a8cd4a9ecb175926c85d75cfd01
Kristina Nylander <kristina.nylander@gmail.com>
2017-12-03 12:27:28 +0100

    Solution for day3 pt2 in progress
    Something is wrong with walking one spiral round. Proper handling of
    stop-case is missing.

--- lib/day3.ex
+++ lib/day3.ex
@@ -16,6 +16,11 @@ defmodule Day3 do
     steps_to_core + steps_to_side_middle
   end
 
+  def spiral_memory_pt2(number) do
+    map = %{{0, 0} => 1}
+    start_spiral_walk(map, {0, 0}, 2, number)
+  end
+
   defp calculate_steps_to_side_middle(number, side_length) when number == (side_length * side_length) do
     div(side_length - 1, 2)
   end
@@ -34,5 +39,43 @@ defmodule Day3 do
   defp find_closest_corner(number, corners) do
     corners |> Enum.map(fn (x) -> {abs(x - number), x} end) |> Enum.min
   end
+
+  defp start_spiral_walk(map, prev_step, steps, number) do
+    {new_map, new_step} = 
+      {map, prev_step} |> walk(1, {1, 0})
+                       |> walk(steps, {0, 1})
+                       |> walk(steps, {-1, 0})
+                       |> walk(steps, {0, -1})
+                       |> walk(steps, {1, 0})
+
+    if new_map[new_step] > number do
+      {new_map, new_step}
+    else
+      start_spiral_walk(new_map, new_step, steps + 2, number)
+    end
+  end
+
+  defp walk({map, step}, 0, _), do: {map, step}
+  defp walk({map, {prev_x, prev_y}}, side, {dir_x, dir_y}) do
+    IO.puts "prev step: #{inspect(prev_x, prev_y)}"
+    new_step = {prev_x + dir_x, prev_y + dir_y}
+    new_value = get_values(map, new_step)
+    res = {Map.put(map, new_step, new_value), new_step}
+    IO.puts "After step {dir_x, dir_y} %{inspect({dir_x, dir_y})}: #{inspect(res)}"
+    res
+
+  end
+
+  defp get_values(map, {x, y}) do
+    Map.get(map, {x-1, y}, 0) +
+      Map.get(map, {x-1, y + 1}, 0) +
+      Map.get(map, {x-1, y - 1}, 0) +
+      Map.get(map, {x, y - 1}, 0) +
+      Map.get(map, {x, y + 1}, 0) +
+      Map.get(map, {x + 1, y - 1}, 0) +
+      Map.get(map, {x + 1, y + 1}, 0) +
+      Map.get(map, {x + 1, y}, 0)
+  end
+    
 
 end
