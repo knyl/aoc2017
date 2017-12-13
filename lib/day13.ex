b7df46f415d32ea9e996c287f23ba4dbafd14955
Kristina Nylander <kristina.nylander@gmail.com>
2017-12-13 08:04:54 +0100

    Add solution for pt2 day 13

--- lib/day13.ex
+++ lib/day13.ex
@@ -5,17 +5,26 @@ defmodule Day13 do
   end
 
   def packet_scanners_pt2(input) do
-    visit(input)
+    visit_delay(input)
   end
 
-  defp visit(input, severity \\ 0)
-  defp visit([], severity), do: severity
-  defp visit([{depth, range} | rest], severity) do
+  defp visit(input, offset \\ 0, severity \\ 0)
+  defp visit([], _, severity), do: severity
+  defp visit([{depth, range} | rest], offset, severity) do
     at_top = (range - 1) * 2
-    if rem(depth, at_top) == 0 do
-      visit(rest, severity + depth * range)
+    if rem(depth + offset, at_top) == 0 do
+      visit(rest, offset, severity + (offset + depth) * range)
     else
-      visit(rest, severity)
+      visit(rest, offset, severity)
     end
   end
+
+  defp visit_delay(input, delay \\ 0) do
+    if visit(input, delay) == 0 do
+      delay
+    else
+      visit_delay(input, delay + 1)
+    end
+  end
+
 end
